7fbdfef4aed1bb8f8e95fd57ffdd6d57ec42f42b
utsubo256 <greeeentea256@gmail.com>
2025-02-19 12:05:56 +0900

    不要なコメントを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,6 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      # format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
       format.html { redirect_to books_url, notice: t('.success') }
       format.json { head :no_content }
     end
