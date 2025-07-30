73992d0d2bb6c58bcaa4284963588c621144a988
utsubo256 <greeeentea256@gmail.com>
2025-07-30 15:26:12 +0900

    日報詳細ページを追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,4 +4,8 @@ class ReportsController < ApplicationController
   def index
     @reports = Report.order(:id).page(params[:page])
   end
+
+  def show
+    @report = Report.find(params[:id])
+  end
 end
