2b37f2ac1597a9670e62a17df6b90d818d1f6bf7
utsubo256 <greeeentea256@gmail.com>
2025-08-01 11:32:23 +0900

    日報作成機能を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,4 +8,23 @@ class ReportsController < ApplicationController
   def show
     @report = Report.find(params[:id])
   end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content).merge(user: current_user)
+  end
 end
