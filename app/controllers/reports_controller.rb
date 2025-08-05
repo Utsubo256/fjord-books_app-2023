feb89b4d74402d4ec165060b43ef81a2c7d0c6c6
utsubo256 <greeeentea256@gmail.com>
2025-08-05 15:37:05 +0900

    日報編集機能を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update]
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
-  def show
-    @report = Report.find(params[:id])
-  end
+  def show; end
 
   def new
     @report = Report.new
@@ -22,7 +21,21 @@ class ReportsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_report
+    @report = Report.find(params[:id])
+  end
 
   def report_params
     params.require(:report).permit(:title, :content).merge(user: current_user)
