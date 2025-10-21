2cdd10857df3c165e9a3676e53d80067eba99799
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2025-10-21 16:33:22 +0900

    コメント一覧を昇順に並び替えるよう修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,11 +7,11 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @commentable.comments.build(comment_params.merge(user: current_user))
-    @comments = @commentable.comments.preload(:user)
 
     if @comment.save
       redirect_to polymorphic_url(@commentable), notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
+      @comments = @commentable.comments.preload(:user).order(created_at: :asc)
       flash.now[:alert] = t('controllers.common.notice_create_failure', name: Comment.model_name.human)
       render "#{@commentable.model_name.plural}/show", status: :unprocessable_entity
     end
