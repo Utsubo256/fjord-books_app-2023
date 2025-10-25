a8cda24f24408f88e85422b784f9986fadb1fc67
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2025-10-25 23:35:39 +0900

    Userの名前またはメールアドレスを表示するロジックをモデルへ移動

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   end
   has_many :reports, dependent: :destroy
   has_many :comments, dependent: :nullify
+
+  def name_or_email
+    name.presence || email
+  end
 end
